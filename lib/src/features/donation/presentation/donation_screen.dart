d8a2d45ff216c155462e75a8e9a9c7406132838d
Ayesha Zafar <Ayeshazafar594@gmail.com>
2026-09-19 17:19:04 +0500

    fix: resolved critical unlinked provider import references to stabilize compilation

--- lib/src/features/donation/presentation/donation_screen.dart
+++ lib/src/features/donation/presentation/donation_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import '../../../core/supabase_setup.dart';
+import '../../home/providers/animal_provider.dart';
 
 import '../../../core/models/animal.dart';
 
